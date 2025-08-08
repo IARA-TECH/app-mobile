c12051cd66d0a0e8dd9f8bb9ddc21a629bf75b1d
Cintya Rios <cintya.christovam@germinare.org.br>
2025-08-08 09:13:59 -0300

    feat(app-iara): melhorar UI da tela de login

--- app/src/main/java/com/mobile/app_iara/ui/inicio/InitiationActivity.kt
+++ app/src/main/java/com/mobile/app_iara/ui/inicio/InitiationActivity.kt
@@ -1,11 +1,14 @@
 package com.mobile.app_iara.ui.inicio
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mobile.app_iara.R
+import android.content.Intent
+
 
 class InitiationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,12 @@ class InitiationActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnIniciar = findViewById<Button>(R.id.btnLoginInicio)
+        btnIniciar.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
