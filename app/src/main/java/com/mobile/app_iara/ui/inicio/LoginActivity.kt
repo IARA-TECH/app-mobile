99816e78e2172ea7d2d5d3698515049afe3299cc
Brenosilva13 <breno.souza@germinare.org.br>
2025-09-04 08:08:11 -0300

    feat(app-iara): implementa tela de erro para falta de conexão wifi .

--- app/src/main/java/com/mobile/app_iara/ui/inicio/LoginActivity.kt
+++ app/src/main/java/com/mobile/app_iara/ui/inicio/LoginActivity.kt
@@ -2,23 +2,29 @@ package com.mobile.app_iara.ui.inicio
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Html
-import android.text.InputType
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
-import android.widget.Button
 import android.widget.ImageButton
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.mobile.app_iara.R
+import com.mobile.app_iara.ui.erros.ErroWifiActivity
+import com.mobile.app_iara.utils.NetworkUtils
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 🔍 Verifica conexão antes de carregar layout
+        if (!NetworkUtils.isInternetAvailable(this)) {
+            startActivity(Intent(this, ErroWifiActivity::class.java))
+            finish()
+            return
+        }
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -35,11 +41,11 @@ class LoginActivity : AppCompatActivity() {
         btnVoltar.setOnClickListener {
             val intent = Intent(this, InitiationActivity::class.java)
             startActivity(intent)
+            finish()
         }
 
         btnToggle.setOnClickListener {
             senhaVisivel = !senhaVisivel
-
             if (senhaVisivel) {
                 edtSenha.transformationMethod = HideReturnsTransformationMethod.getInstance()
                 btnToggle.setImageResource(R.drawable.olhoaberto)
@@ -47,9 +53,7 @@ class LoginActivity : AppCompatActivity() {
                 edtSenha.transformationMethod = PasswordTransformationMethod.getInstance()
                 btnToggle.setImageResource(R.drawable.olhofechado)
             }
-
             edtSenha.setSelection(edtSenha.text?.length ?: 0)
         }
-
     }
 }
