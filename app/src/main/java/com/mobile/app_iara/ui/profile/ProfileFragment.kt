049a6aca9e30b0148f5ef68dc80890c58e8715a7
Cintya Rios <cintya.christovam@germinare.org.br>
2025-09-14 15:42:31 -0300

    feat: adicionando o modal para confirmar saida do usuario

--- app/src/main/java/com/mobile/app_iara/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/mobile/app_iara/ui/profile/ProfileFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.card.MaterialCardView
 import com.google.firebase.auth.FirebaseAuth
 import com.mobile.app_iara.ui.inicio.LoginActivity
@@ -23,15 +25,9 @@ class ProfileFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
         val btnSair = view.findViewById<MaterialCardView>(R.id.btnSair)
+
         btnSair.setOnClickListener {
-            val prefs = requireActivity().getSharedPreferences("user_prefs", 0)
-            prefs.edit().putBoolean("is_logged_in", false).apply()
-
-            FirebaseAuth.getInstance().signOut()
-
-            val intent = Intent(requireContext(), LoginActivity::class.java)
-            startActivity(intent)
-            requireActivity().finish()
+            confirmarSaida()
         }
 
         val btnTermos = view.findViewById<MaterialCardView>(R.id.btnTermsandconditions)
@@ -53,4 +49,38 @@ class ProfileFragment : Fragment() {
         }
         return view
     }
+
+    private fun confirmarSaida() {
+        val builder = AlertDialog.Builder(requireContext())
+        val inflater = layoutInflater
+        val view = inflater.inflate(R.layout.dialog_confirmar_saida, null)
+        builder.setView(view)
+
+        val dialog = builder.create()
+        dialog.show()
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        val width = (resources.displayMetrics.widthPixels * 0.8).toInt()
+        dialog.window?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        val btnSair = view.findViewById<Button>(R.id.btnSairDialog)
+        val btnCancelar = view.findViewById<Button>(R.id.btnCancelarDialog)
+
+        btnSair.setOnClickListener {
+            val prefs = requireActivity().getSharedPreferences("user_prefs", 0)
+            prefs.edit().putBoolean("is_logged_in", false).apply()
+
+            FirebaseAuth.getInstance().signOut()
+
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+
+            dialog.dismiss()
+        }
+
+        btnCancelar.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+
 }
