b2427b7e893bab341866fe43e9626bedd0e80d89
ciintyaarioss <cintya.christovam@germinare.org.br>
2025-10-02 22:05:31 -0300

    feat: Adicionando a tela de gestão dos funcionários

---
+++ app/src/main/java/com/mobile/app_iara/ui/management/collaborator/CollaboratorModal.kt
@@ -0,0 +1,9 @@
+package com.mobile.app_iara.ui.management.collaborator
+
+data class CollaboratorModal(
+    val id: String,
+    val name: String,
+    val email: String,
+    val role: String,
+    val urlPhoto: String?
+)
