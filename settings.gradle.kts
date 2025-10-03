2c446f5e045ffb68cdd25b4d64f895aaa50cbb67
Brenosilva13 <157593253+Brenosilva13@users.noreply.github.com>
2025-10-03 11:46:38 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        // ADICIONE ESTA LINHA AQUI
         maven { url = uri("https://jitpack.io") }
     }
 }
