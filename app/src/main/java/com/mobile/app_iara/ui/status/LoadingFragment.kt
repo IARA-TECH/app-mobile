a7018f47ff89fa81a4222aa6221878394a109bc3
Cintya Rios <cintya.christovam@germinare.org.br>
2025-10-15 08:47:53 -0300

    feat: Adicionando a tela de loading

---
+++ app/src/main/java/com/mobile/app_iara/ui/status/LoadingFragment.kt
@@ -0,0 +1,26 @@
+package com.mobile.app_iara.ui.status
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.airbnb.lottie.LottieAnimationView
+import com.mobile.app_iara.R
+
+class LoadingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_loading, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Controle do tempo do loading
+    }
+
+}
