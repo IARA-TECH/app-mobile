fb1f20df4e2b2e9bd1f947c75a0cb0ad484dfd97
ciintyaarioss <cintya.christovam@germinare.org.br>
2025-11-01 20:03:29 -0300

    fix: arrumando erro de criacao de linha

--- app/src/main/java/com/mobile/app_iara/data/model/LinesData.kt
+++ app/src/main/java/com/mobile/app_iara/data/model/LinesData.kt
@@ -2,11 +2,11 @@ package com.mobile.app_iara.data.model
 
 data class Line(
     val name: String,
-    val type: LineType
+    val lineType: LineType
 )
 
 data class LineType(
     val id: String,
     val name: String,
-    val createdDate: String
+    val createdAt: String
 )
